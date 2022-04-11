aa1a3c371e5159abb210818c560c19dc0224a09e
TECH4ED-PC5 <TECH4ED-PC5@TECH4ED_5>
2022-04-11 15:40:48 +0800

    Add project files.

---
+++ PantawidPamilya/4ps/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PantawidPamilya._4ps
+{
+    public class Class
+    {
+        public int ID { get; set; }
+        public string Barangay { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public int Beneficiaries  { get; set; }
+    }
+}
---
+++ PantawidPamilya/Data/PantawidPamilyaContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PantawidPamilya._4ps;
+
+namespace PantawidPamilya.Data
+{
+    public class PantawidPamilyaContext : DbContext
+    {
+        public PantawidPamilyaContext (DbContextOptions<PantawidPamilyaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PantawidPamilya._4ps.Class> Class { get; set; }
+    }
+}
